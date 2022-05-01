ccc815d1031f81abb03cb45ea29264d9732938a1
alchemistOfWeb <nikitakzntcv@gmail.com>
2022-05-01 20:02:15 +0400

    add test routes for reactapp

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// "npm i -D react-router-dom" - write for install router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import './index.css';
-import App from './App';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import NoPage from './pages/NoPage';
+
 import reportWebVitals from './reportWebVitals';
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ reactapp/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => {
+    // window.ymaps.ready(()=>{
+    //     console.log('hello world!');
+    // })
+    return <h1>Home</h1>;
+};
+  
+export default Home;
